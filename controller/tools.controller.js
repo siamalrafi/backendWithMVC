d747044103e3243abc4aeaa8642f53cd1c00fa0d
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-15 19:38:44 +0600

    This is the redirect system in express

--- controller/tools.controller.js
+++ controller/tools.controller.js
@@ -2,7 +2,10 @@ module.exports.getAllTools = (req, res, next) => {
     const { ip, query, params, body, headers } = req;
     // console.log(ip, query, params, body, headers);
 
-    res.download(__dirname + "/../index.js")
+
+     res.redirect("/login")
+    
+
 }
 
 module.exports.saveATools = (req, res) => {
